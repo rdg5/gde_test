69542023766af4c2962b0a7935449e045ee70f95
fox <box@foxs-MacBook-Pro-2.local>
2025-04-14 17:18:14 +0200

    test

--- src/tests/integration/app.test.js
+++ src/tests/integration/app.test.js
@@ -1,31 +1,19 @@
-const request = require("supertest");
-const { app, products } = require("../../app");
+const request = require('supertest')
+const { app, products } = require("../../app")
 
-describe("Integration Test - Express /products", () => {
-  beforeEach(() => {
-    products[0].price = 1000;
-    products[1].price = 25;
-  });
+describe("REST API Integration tests", () => {
 
-  it("GET /products => returns 2 items", async () => {
-    const res = await request(app).get("/products");
-    expect(res.status).toBe(200);
-    expect(res.body).toHaveLength(2);
-  });
+    it("GET /products returns two elements and 200 status", async () => {
+       const response = await request(app).get("/products")
+       expect(response.status).toBe(200)
+       expect(response.body).toHaveLength(2)
+    })
 
-  it("POST /products/1/discount => updates price", async () => {
-    const res = await request(app)
-      .post("/products/1/discount")
-      .send({ percent: 50 });
-    expect(res.status).toBe(200);
-    expect(res.body.price).toBe(500);
-  });
-
-  it("POST /products/1/discount => 400 if discount invalid", async () => {
-    const res = await request(app)
-      .post("/products/1/discount")
-      .send({ percent: 150 });
-    expect(res.status).toBe(400);
-    expect(res.body).toMatchObject({ error: "Invalid discount percentage" });
-  });
-});
+    it("POST /products/1/discount updates the price", async () => {
+        const response = await request(app)
+        .post("/products/1/discount")
+        .send({percent: 50})
+        expect(response.status).toBe(200)
+        expect(response.body.price).toBe(500)
+    })
+})
